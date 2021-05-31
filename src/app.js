8f9e08619296003fa9aeeadbec50de657bf52cb4
Esteban De la Rosa <gabbanaesteban@gmail.com>
2021-05-30 21:19:41 -0400

    Add docs

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@
 
 const express = require("express")
 const helmet = require("helmet")
-
+const cors = require("cors")
+const swaggerUi = require("swagger-ui-express")
 const router = require("./routes/router")
+
+const swaggerDocument = require("../docs/swagger.json")
 
 const app = express()
 
 app.use(helmet())
+app.use(cors())
 
-app.use('/api', router)
+app.use("/api", router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 module.exports = app
